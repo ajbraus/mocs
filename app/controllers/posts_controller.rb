74a53e81d41e2c4c9dd858b5b7ba540a24e815d6
ajbraus <ajbraus@gmail.com>
2013-07-30 16:36:32 -0500

    fixing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,14 @@ class PostsController < ApplicationController
       end
       @posts = Post.search(params[:search], with: { published: true }, :page => params[:page], :per_page => 10)
       @posts_by_activity = Post.search(params[:search], with: { published: true }, order: 'last_touched asc', :page => params[:page], :per_page => 10)
-      #@posts_by_followers = @mocs.sort_by { |m| m.followers.count }
+      # @posts_by_participants = @mocs.sort_by { |m| m.commitments.count }
     else 
       if user_signed_in?
         @posts_by_state = Post.where(state: current_user.state, published: true).order('published_at desc').paginate(:page => params[:page], :per_page => 10)
       end
       @posts = Post.where(published: true).order('published_at desc').paginate(:page => params[:page], :per_page => 10)
       @posts_by_activity = Post.order( "last_touched asc" ).paginate(:page => params[:page], :per_page => 10)
-      #@posts_by_followers = Post.sort_by { |m| m.followers.count }
+      # @posts_by_participants = Post.sort_by { |m| m.commitments.count }
     end
     @trending_mocs = Post.first(3)
     @trending_tags = Tag.first(20)
