c4e652792d82daa8f5f276f4964b1aeacf144374
ajbraus <ajbraus@gmail.com>
2013-08-07 10:17:28 -0500

    blahg

--- db/migrate/20130806035307_create_goals_posts_table.rb
+++ db/migrate/20130806035307_create_goals_posts_table.rb
@@ -7,18 +7,6 @@ class CreateGoalsPostsTable < ActiveRecord::Migration
     add_index :goals_posts, [:post_id, :goal_id]
     add_index :goals_posts, [:goal_id, :post_id]
 
-    Post.all.each do |p|
-      if p.goal == "Patient Outcomes"
-        p.goals << Goal.find(1)
-      elsif p.goal == "Patient Satisfaction"
-        p.goals << Goal.find(2)
-      elsif p.goal == "Work Process"
-        p.goals << Goal.find(3)
-      else
-        p.goals << Goal.find(2)
-      end
-    end
-
     remove_column :posts, :goal
   end
 
