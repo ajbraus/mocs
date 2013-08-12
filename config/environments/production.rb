7c3082d3f4fd68188f21a4d0ad2bdfc3e5ce7f45
ajbraus <ajbraus@gmail.com>
2013-08-12 11:47:04 -0500

    blah

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Mocs::Application.configure do
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD']
   }
-  
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
