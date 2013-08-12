c35d3ef934c33ba0c15d19fdb34c64eb1bd4e967
ajbraus <ajbraus@gmail.com>
2013-08-12 12:09:02 -0500

    blah

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ Mocs::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.action_mailer.default_url_options = { :host => 'www.mocsfordocs.org' }
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
