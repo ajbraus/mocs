b9213d9962a73c229e294c3fb698c535096b09f1
ajbraus <ajbraus@gmail.com>
2013-09-06 09:25:48 -0500

    blah

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -10,6 +10,8 @@ $(".next").click(function(){
   //activate next step on progressbar using the index of next_fs
   $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
+  $('body').scrollTop(0);
+
   current_fs.hide();
 
   //show the next fieldset
@@ -48,3 +50,62 @@ $("#progressbar li").click(function(){
   $('fieldset').eq(step).fadeIn();
 });
 
+
+if (!!$('#post_title').val() && 
+    !!$("#post_goal_id option:selected").val().length > 0) 
+{
+  $("#save").removeClass("disabled");
+}
+
+if ($('#post_title').length > 0 && 
+    $("#post_goal_id option:selected").val().length > 0 &&
+    $("#post_expected_time").val().length > 0 &&
+    $("#post_duration").val().length > 0 &&
+    $("#post_desc").val().length > 0 &&
+    $("#post_price_in_dollars").val().length > 0 &&
+    $("#post_info").val().length > 0 &&
+    $("#post_baseline").val().length > 0 &&
+    $("#post_plan_do").val().length > 0 &&
+    $("#post_post_test").val().length > 0 &&
+    $("#post_wrap_up").val().length > 0)
+    // $("#post_credits").val().length > 0 &&
+    // $("#post_begins_on").val().length > 0 &&
+    // $("#post_ends_on").val().length > 0 &&
+{
+  $('#publish').val("Publish!").removeClass("disabled").addClass("btn-gold");
+}
+
+
+$("form").keyup(function(){
+  if ($('#post_title').val().length > 0 && 
+      $("#post_goal_id option:selected").val().length > 0) 
+  {
+    $("#save").addClass("btn-success").removeClass("disabled");
+    $("#saveTip").remove();
+  }
+
+  if ($('#post_title').length > 0 && 
+      $("#post_goal_id option:selected").val().length > 0 &&
+      $("#post_expected_time").val().length > 0 &&
+      $("#post_duration").val().length > 0 &&
+      $("#post_desc").val().length > 0 &&
+      $("#post_price_in_dollars").val().length > 0 &&
+      // $("#post_credits").val().length > 0 &&
+      // $("#post_begins_on").val().length > 0 &&
+      // $("#post_ends_on").val().length > 0 &&
+      $("#post_info").val().length > 0 &&
+      $("#post_baseline").val().length > 0 &&
+      $("#post_plan_do").val().length > 0 &&
+      $("#post_post_test").val().length > 0 &&
+      $("#post_wrap_up").val().length > 0)
+  {
+    $('#publish').val("Publish!").removeClass("disabled").addClass("btn-gold");
+  }
+});
+
+$(function() {
+  $( ".datepicker" ).datepicker({ 
+    dateFormat: "dd-mm-yy",
+    minDate: new Date()
+   });
+});
